491243a64345b20566d22f99298d4f7eaf8c1120
Mukesh <onlymukesh20@gmail.com>
2021-05-22 17:36:13 +0530

    second commit

---
+++ src/test/java/com/mukesh/SampleAppTest.java
@@ -0,0 +1,20 @@
+package com.mukesh;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SampleAppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
